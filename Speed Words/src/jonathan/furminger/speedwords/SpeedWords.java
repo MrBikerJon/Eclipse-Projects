2b2e5d0e1db090395b0e0ffe4e2604d316bd7553
Jonathan Furminger <58958324+jonathanfurminger@users.noreply.github.com>
2022-01-19 16:14:34 +0000

    SpeedWords 2-16

--- Speed Words/src/jonathan/furminger/speedwords/SpeedWords.java
+++ Speed Words/src/jonathan/furminger/speedwords/SpeedWords.java
@@ -1,16 +1,22 @@
 package jonathan.furminger.speedwords;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
+import jonathan.furminger.mycomponents.ScorePanel;
 import jonathan.furminger.mycomponents.TitleLabel;
 
 public class SpeedWords extends JFrame {
 
 	private static final long serialVersionUID = 1L;
+	public static final Color TAN = new Color(222, 191, 168);
+	private ScorePanel scorePanel = new ScorePanel(0, TAN);
 
 	public static void main(String[] args) {
 		try {
@@ -39,5 +45,27 @@ public class SpeedWords extends JFrame {
 	private void initGUI() {
 		TitleLabel titleLabel = new TitleLabel("Speed Words");
 		add(titleLabel, BorderLayout.PAGE_START);
+		
+		// main panel
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
+		mainPanel.setBackground(TAN);
+		add(mainPanel, BorderLayout.CENTER);
+		
+		// left panel
+		JPanel leftPanel = new JPanel();
+		leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
+		leftPanel.setBackground(TAN);
+		add(leftPanel);
+		
+		// score panel
+		leftPanel.add(scorePanel);
+		
+		// timer panel
+		
+		// game panel
+		
+		// text area
+		
 	}
 }
