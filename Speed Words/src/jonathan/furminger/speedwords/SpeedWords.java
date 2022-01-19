2cba2b787e5aad6295417055bf49dedd5768f543
Jonathan Furminger <58958324+jonathanfurminger@users.noreply.github.com>
2022-01-19 14:27:51 +0000

    started Speed Words

---
+++ Speed Words/src/jonathan/furminger/speedwords/SpeedWords.java
@@ -0,0 +1,39 @@
+package jonathan.furminger.speedwords;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+import jonathan.furminger.mycomponents.TitleLabel;
+
+public class SpeedWords extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		try {
+			String className = UIManager.getCrossPlatformLookAndFeelClassName();
+			UIManager.setLookAndFeel(className);
+		} catch (Exception e) {
+			
+		}
+
+	}
+	
+	public SpeedWords() {
+		initGUI();
+		
+		setTitle("Speed Words");
+		setResizable(false);
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	private void initGUI() {
+		TitleLabel titleLabel = new TitleLabel("Speed Words");
+		add(titleLabel, BorderLayout.PAGE_START);
+	}
+}
