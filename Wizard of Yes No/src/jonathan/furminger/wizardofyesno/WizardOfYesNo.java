2ca4ef7510124e7b8daa799113b75018b47cfd86
Jonathan Furminger <58958324+jonathanfurminger@users.noreply.github.com>
2022-01-22 21:12:50 +0000

    Added Wizard of Yes No

---
+++ Wizard of Yes No/src/jonathan/furminger/wizardofyesno/WizardOfYesNo.java
@@ -0,0 +1,18 @@
+package jonathan.furminger.wizardofyesno;
+
+import javax.swing.JFrame;
+
+public class WizardOfYesNo extends JFrame {
+
+	
+	public WizardOfYesNo() {
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args) {
+		new WizardOfYesNo();
+	}
+
+
+}
