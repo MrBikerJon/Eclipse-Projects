8b9c4c0498ccbb0bcb41844b90d39ddc4a83386e
Jonathan Furminger <58958324+jonathanfurminger@users.noreply.github.com>
2022-01-22 21:35:26 +0000

    Wizard of Yes No 1-11

--- Wizard of Yes No/src/jonathan/furminger/wizardofyesno/WizardOfYesNo.java
+++ Wizard of Yes No/src/jonathan/furminger/wizardofyesno/WizardOfYesNo.java
@@ -1,18 +1,36 @@
 package jonathan.furminger.wizardofyesno;
 
+import java.awt.Font;
+import java.util.Random;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class WizardOfYesNo extends JFrame {
+	
+	private static final String[] ANSWER = {"Yes", "No"};
 
 	
 	public WizardOfYesNo() {
+		Random rand = new Random();
+		int numberOfAnswers = ANSWER.length;
+		int pick = rand.nextInt(numberOfAnswers);
+		String answer = ANSWER[pick];
+		
+		JLabel label = new JLabel();
+		label.setText(answer);
+		Font font = new Font("Times New Roman", Font.PLAIN, 18);
+		label.setFont(font);
+		add(label);
+		
+		setSize(200, 100);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
 	}
 	
 	public static void main(String[] args) {
 		new WizardOfYesNo();
 	}
-
 
 }
