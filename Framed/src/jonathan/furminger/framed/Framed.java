0ca75637ff907b5674631f9c12362d7a34b1542a
Jonathan Furminger <58958324+jonathanfurminger@users.noreply.github.com>
2022-01-25 17:08:24 +0000

    Framed 4-9

--- Framed/src/jonathan/furminger/framed/Framed.java
+++ Framed/src/jonathan/furminger/framed/Framed.java
@@ -1,9 +1,12 @@
 package jonathan.furminger.framed;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.UIManager;
+
+import jonathan.furminger.mycomponents.TitleLabel;
 
 public class Framed extends JFrame {
 
@@ -25,12 +28,19 @@ public class Framed extends JFrame {
 	}
 	
 	public Framed() {
+		initGUI();
+		
 		setTitle("Framed");
 		setResizable(false);
 		pack();
 		setLocationRelativeTo(null);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public void initGUI() {
+		TitleLabel titleLabel = new TitleLabel("Framed");
+		add(titleLabel, BorderLayout.PAGE_START);
 	}
 
 }
