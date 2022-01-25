7027827a6bafdba7d47192ee9e553153247fd2f7
Jonathan Furminger <58958324+jonathanfurminger@users.noreply.github.com>
2022-01-25 17:23:10 +0000

    Framed 4-12

--- Framed/src/jonathan/furminger/framed/Framed.java
+++ Framed/src/jonathan/furminger/framed/Framed.java
@@ -2,8 +2,10 @@ package jonathan.furminger.framed;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.GridLayout;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.UIManager;
 
 import jonathan.furminger.mycomponents.TitleLabel;
@@ -11,6 +13,8 @@ import jonathan.furminger.mycomponents.TitleLabel;
 public class Framed extends JFrame {
 
 	private static final long serialVersionUID = 1L;
+	private static final int GRIDSIZE = 3;
+	private LightButton[][] lightButton = new LightButton[GRIDSIZE][GRIDSIZE];
 
 	public static void main(String[] args) {
 
@@ -41,6 +45,18 @@ public class Framed extends JFrame {
 	public void initGUI() {
 		TitleLabel titleLabel = new TitleLabel("Framed");
 		add(titleLabel, BorderLayout.PAGE_START);
+		
+		
+		JPanel centerPanel = new JPanel();
+		centerPanel.setLayout(new GridLayout(GRIDSIZE, GRIDSIZE));
+		add(centerPanel, BorderLayout.CENTER);
+		
+		for(int row = 0; row < GRIDSIZE; row++) {
+			for(int col = 0; col < GRIDSIZE; col++) {
+				lightButton[row][col] = new LightButton(row, col);
+				centerPanel.add(lightButton[row][col]);
+			}
+		}
 	}
 
 }
