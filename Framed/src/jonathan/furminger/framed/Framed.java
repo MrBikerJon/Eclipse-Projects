81e42a462ab8476ab9a472b099ae661fb2fb6b8c
Jonathan Furminger <58958324+jonathanfurminger@users.noreply.github.com>
2022-01-25 23:09:37 +0000

    Framed 4-19

--- Framed/src/jonathan/furminger/framed/Framed.java
+++ Framed/src/jonathan/furminger/framed/Framed.java
@@ -3,6 +3,8 @@ package jonathan.furminger.framed;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -40,6 +42,8 @@ public class Framed extends JFrame {
 		setLocationRelativeTo(null);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		newGame();
 	}
 	
 	public void initGUI() {
@@ -54,9 +58,81 @@ public class Framed extends JFrame {
 		for(int row = 0; row < GRIDSIZE; row++) {
 			for(int col = 0; col < GRIDSIZE; col++) {
 				lightButton[row][col] = new LightButton(row, col);
+				lightButton[row][col].addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						LightButton button = (LightButton) e.getSource();
+						int row = button.getRow();
+						int col = button.getCol();
+						toggleLights(row, col);
+					}
+			});
 				centerPanel.add(lightButton[row][col]);
 			}
 		}
+	}
+	
+	public void toggleLights(int row, int col) {
+		lightButton[row][col].toggle();
+		// top left corner
+		if(row==0 && col==0) {
+			lightButton[0][1].toggle();
+			lightButton[1][1].toggle();
+			lightButton[1][0].toggle();
+		}
+		//top right corner
+		else if(row==0 && col==2) {
+			lightButton[0][1].toggle();
+			lightButton[1][1].toggle();
+			lightButton[1][2].toggle();
+		}
+		// bottom left corner
+		else if(row==2 && col==0) {
+			lightButton[1][0].toggle();
+			lightButton[1][1].toggle();
+			lightButton[2][1].toggle();
+		}
+		// bottom right corner
+		else if(row==2 && col==2) {
+			lightButton[2][1].toggle();
+			lightButton[1][1].toggle();
+			lightButton[1][2].toggle();
+		}
+		// top row middle
+		else if(row==0 && col==1) {
+			lightButton[0][0].toggle();
+			lightButton[0][2].toggle();
+		}	
+		// bottom row middle
+		else if(row==2 && col==1) {
+			lightButton[2][0].toggle();
+			lightButton[2][2].toggle();
+		}
+		// left side middle
+		else if(row==1 && col==0) {
+			lightButton[0][0].toggle();
+			lightButton[2][0].toggle();
+		}
+		// right side middle
+		else if(row==1 && col==2) {
+			lightButton[0][2].toggle();
+			lightButton[2][2].toggle();
+		}
+		//center
+		else if(row==1 && col==1) {
+			lightButton[0][1].toggle();
+			lightButton[1][0].toggle();
+			lightButton[1][2].toggle();
+			lightButton[2][1].toggle();
+		}
+	}
+	
+	private void newGame() {
+		for(int row = 0; row < GRIDSIZE; row++) {
+			for(int col = 0; col < GRIDSIZE; col++) {
+				lightButton[row][col].turnOn();
+			}
+		}
+		lightButton[1][1].toggle();
 	}
 
 }
