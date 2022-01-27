a53df24d5b820e25eb2a21ecb923660481b43f03
Jonathan Furminger <58958324+jonathanfurminger@users.noreply.github.com>
2022-01-27 23:04:44 +0000

    Sliding Tiles 6-2

--- Sliding Tiles/src/jonathan/furminger/slidingtiles/SlidingTiles.java
+++ Sliding Tiles/src/jonathan/furminger/slidingtiles/SlidingTiles.java
@@ -1,12 +1,50 @@
 package jonathan.furminger.slidingtiles;
 
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+import jonathan.furminger.mycomponents.TitleLabel;
 
 public class SlidingTiles extends JFrame {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static final long serialVersionUID = 1L;
 
+
+	public SlidingTiles() {
+		initGUI();
+		setTitle("Sliding Tiles");
+		setResizable(false);
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+	}
+	
+	public static void main(String[] args) {
+
+		try {
+			String className = UIManager.getCrossPlatformLookAndFeelClassName();
+			UIManager.setLookAndFeel(className);
+		} catch (Exception e) { }
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				new SlidingTiles();
+			}
+		});
+		
+
+	}
+	
+	
+	private void initGUI() {
+		TitleLabel titleLabel = new TitleLabel("Sliding Tiles");
+		add(titleLabel, BorderLayout.LINE_START);
+		
 	}
 
 }
