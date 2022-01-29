ebb5542173e717d20e64be807e4dbc6c446f4c62
Jonathan Furminger <58958324+jonathanfurminger@users.noreply.github.com>
2022-01-29 22:16:29 +0000

    Project 7 - Maze Generator - first commit

--- Maze Generator/src/jonathan/furminger/mazegenerator/MazeGenerator.java
+++ Maze Generator/src/jonathan/furminger/mazegenerator/MazeGenerator.java
@@ -1,5 +1,7 @@
 package jonathan.furminger.mazegenerator;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
 
 import jonathan.furminger.mycomponents.TitleLabel;
@@ -23,6 +25,10 @@ public class MazeGenerator extends JFrame {
 		setLocationRelativeTo(null);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	private void initGUI() {
+		add(titleLabel, BorderLayout.PAGE_START);
 	}
 
 }
