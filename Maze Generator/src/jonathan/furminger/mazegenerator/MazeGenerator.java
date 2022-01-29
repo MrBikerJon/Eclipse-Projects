af69b049695176d19ca13ec2bed269fda0c88e3b
Jonathan Furminger <58958324+jonathanfurminger@users.noreply.github.com>
2022-01-29 22:34:58 +0000

    Project 7 - Maze Generator 7-5

--- Maze Generator/src/jonathan/furminger/mazegenerator/MazeGenerator.java
+++ Maze Generator/src/jonathan/furminger/mazegenerator/MazeGenerator.java
@@ -1,8 +1,10 @@
 package jonathan.furminger.mazegenerator;
 
 import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.UIManager;
 
 import jonathan.furminger.mycomponents.TitleLabel;
 
@@ -13,8 +15,17 @@ public class MazeGenerator extends JFrame {
 	private TitleLabel titleLabel = new TitleLabel("Maze");
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String className = UIManager.getCrossPlatformLookAndFeelClassName();
+		try {
+			UIManager.setLookAndFeel(className);
+		}
+		catch (Exception e) {}
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				new MazeGenerator();
+			}
+		});
 	}
 	
 	public MazeGenerator() {
@@ -25,10 +36,14 @@ public class MazeGenerator extends JFrame {
 		setLocationRelativeTo(null);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
 	}
 	
 	private void initGUI() {
 		add(titleLabel, BorderLayout.PAGE_START);
+		
+		Cell cell = new Cell();
+		add(cell, BorderLayout.CENTER);
 	}
 
 }
