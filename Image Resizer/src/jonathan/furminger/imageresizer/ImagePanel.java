fe9cf0ad8a403cf9675ec7c4f75793013ef79695
Jonathan Furminger <58958324+jonathanfurminger@users.noreply.github.com>
2022-02-11 15:13:22 +0000

    More DIY Java Games - ImageResizer 12-12

--- Image Resizer/src/jonathan/furminger/imageresizer/ImagePanel.java
+++ Image Resizer/src/jonathan/furminger/imageresizer/ImagePanel.java
@@ -10,6 +10,7 @@ import javax.swing.JPanel;
 public class ImagePanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
+	
 	private ImageResizer imageResizer = null;
 	private BufferedImage image = null;
 	
--- Image Resizer/src/jonathan/furminger/imageresizer/ImageResizer.java
+++ Image Resizer/src/jonathan/furminger/imageresizer/ImageResizer.java
@@ -2,9 +2,19 @@ package jonathan.furminger.imageresizer;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JToolBar;
 import javax.swing.UIManager;
 
 import jonathan.furminger.mycomponents.TitleLabel;
@@ -12,6 +22,15 @@ import jonathan.furminger.mycomponents.TitleLabel;
 public class ImageResizer extends JFrame {
 
 	private static final long serialVersionUID = 1L;
+	private static final String IMAGE_LOAD = "/Load16.gif";
+	private static final String IMAGE_SAVE = "/Save16.gif";
+	private static final String IMAGE_SCALE = "/Scale16.gif";
+	private static final String IMAGE_WIDTH = "/Width16.gif";
+	private static final String IMAGE_HEIGHT = "/Height16.gif";
+	private static final String IMAGE_CROP = "/Crop16.gif";
+	private static final String IMAGE_X = "/x16.gif";
+	private static final String IMAGE_Y = "/y16.gif";
+	
 	private ImagePanel imagePanel = new ImagePanel(this);
 
 	public ImageResizer() {
@@ -50,6 +69,27 @@ public class ImageResizer extends JFrame {
 		
 		
 		// toolbar
+		ImageIcon loadIcon = new ImageIcon(getClass().getResource(IMAGE_LOAD));
+		ImageIcon saveIcon = new ImageIcon(getClass().getResource(IMAGE_SAVE));
+		ImageIcon scaleIcon = new ImageIcon(getClass().getResource(IMAGE_SCALE));
+		ImageIcon widthIcon = new ImageIcon(getClass().getResource(IMAGE_WIDTH));
+		ImageIcon heightIcon = new ImageIcon(getClass().getResource(IMAGE_HEIGHT));
+		ImageIcon cropIcon = new ImageIcon(getClass().getResource(IMAGE_CROP));
+		ImageIcon xIcon = new ImageIcon(getClass().getResource(IMAGE_X));
+		ImageIcon yIcon = new ImageIcon(getClass().getResource(IMAGE_Y));
+		
+		JToolBar toolbar = new JToolBar();
+		mainPanel.add(toolbar, BorderLayout.PAGE_START);
+		
+		JButton loadButton = new JButton(loadIcon);
+		loadButton.setToolTipText("Load Image");
+		loadButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				load();
+			}
+		});
+		toolbar.add(loadButton);
+		
 		
 		// scale options
 		
@@ -58,6 +98,19 @@ public class ImageResizer extends JFrame {
 		// image panel
 		mainPanel.add(imagePanel, BorderLayout.CENTER);
 		
+	}
+	
+	private void load() {
+		File file = new File("slidingTilesImage.jpg");
+		try {
+			BufferedImage image = ImageIO.read(file);
+			imagePanel.setImage(image);
+		}
+		catch (IOException e) {
+			String message =  "Could not open the file " + file.getPath();
+			JOptionPane.showMessageDialog(this, message);
+		}
+
 	}
 
 }
