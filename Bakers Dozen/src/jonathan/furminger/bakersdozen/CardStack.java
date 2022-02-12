8b2b7f63b7d2d0ff61e86bc30aebcd5f19c684ca
Jonathan Furminger <58958324+jonathanfurminger@users.noreply.github.com>
2022-02-12 21:51:23 +0000

    Solitaire 13.34

--- Bakers Dozen/src/jonathan/furminger/bakersdozen/CardStack.java
+++ Bakers Dozen/src/jonathan/furminger/bakersdozen/CardStack.java
@@ -1,5 +1,17 @@
 package jonathan.furminger.bakersdozen;
 
+import java.util.ArrayList;
+
 public class CardStack {
 
+	private ArrayList<Card> cards = new ArrayList<>();
+	private int stackX = 0;
+	private int stackY = 0;
+	private int overlap = 0;
+	
+	public CardStack (int stackX, int stackY, int overlap) {
+		this.stackX = stackX;
+		this.stackY = stackY;
+		this.overlap = overlap;
+	}
 }
