8a332a1cb58df4236bd441a3bec28f632ded92f3
Jonathan Furminger <58958324+jonathanfurminger@users.noreply.github.com>
2022-02-12 18:02:57 +0000

    More DIY Java Games - Solitaire 13-13

--- Bakers Dozen/src/jonathan/furminger/bakersdozen/Deck.java
+++ Bakers Dozen/src/jonathan/furminger/bakersdozen/Deck.java
@@ -2,5 +2,24 @@ package jonathan.furminger.bakersdozen;
 
 public class Deck {
 	
-	private static final String[] RANKS = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"};
+	private static final String[] RANKS = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+	private static final String[] SUITSYMBOLS = {"\u2665", "\u2666", "\u2660", "\u2663"};
+	private static final int CARDWIDTH = 30;
+	private static final int CARDHEIGHT = 50;
+	
+	public static String[] getRanks() {
+		return RANKS;
+	}
+	
+	public static String[] getSuitSymbols() {
+		return SUITSYMBOLS;
+	}
+	
+	public static int getCardWidth() {
+		return CARDWIDTH;
+	}
+	
+	public static int getCardHeight() {
+		return CARDHEIGHT;
+	}
 }
--- Bakers Dozen/src/jonathan/furminger/bakersdozen/DrawDeckImage.java
+++ Bakers Dozen/src/jonathan/furminger/bakersdozen/DrawDeckImage.java
@@ -1,6 +1,15 @@
 package jonathan.furminger.bakersdozen;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JOptionPane;
 
 public class DrawDeckImage {
 
@@ -16,6 +25,49 @@ public class DrawDeckImage {
 		BufferedImage image = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_RGB);
 		
 		Graphics g = image.getGraphics();
+		g.setColor(Color.GREEN);
+		g.fillRect(0, 0, imageWidth, imageHeight);
+		
+		Font font = new Font(Font.DIALOG, Font.BOLD, 24);
+		g.setFont(font);
+		FontMetrics fm = g.getFontMetrics(font);
+		
+		for(int row = 0, y = 0; row < 4; row++, y += cardHeight) {
+			for(int col = 0, x = 0; col < 13; col++, x += cardWidth) {
+				g.setColor(Color.WHITE);
+				g.fillRoundRect(x, y, cardWidth-1, cardHeight-1, 8, 8);
+				g.setColor(Color.BLACK);
+				g.drawRoundRect(x, y, cardWidth-1, cardHeight-1, 8, 8);
+				
+				if(row < 2) {
+					g.setColor(Color.RED);
+				}
+				
+				String rank = ranks[col];
+				int rankWidth = fm.stringWidth(rank);
+				int fromLeft = x + (cardWidth / 2) - (rankWidth / 2);
+				int fromTop = y + 20;
+				g.drawString(rank, fromLeft, fromTop);
+				
+				String suit = suits[row];
+				int suitWidth = fm.stringWidth(suit);
+				fromLeft = x + (cardWidth / 2) - (suitWidth / 2);
+				fromTop = y + 45;
+				g.drawString(suit, fromLeft, fromTop);
+			}
+		}
+		
+		String fileName = "cards.png";
+		File file = new File(fileName);
+		try {
+			ImageIO.write(image, "png", file);
+		}
+		catch (IOException e) {
+			String message = fileName + " could not be saved.";
+			JOptionPane.showMessageDialog(null, message);
+		}
+		
+		
 		
 
 	}
