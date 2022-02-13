ece88dd6b4b33c6fc6540a945ef17bf2c21254a0
Jonathan Furminger <58958324+jonathanfurminger@users.noreply.github.com>
2022-02-13 21:12:05 +0000

    Match Three 3-19

---
+++ MatchThree/src/jonathan/furminger/matchthree/MatchThree.java
@@ -0,0 +1,67 @@
+package jonathan.furminger.matchthree;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+
+import jonathan.furminger.mycomponents.ScorePanel;
+import jonathan.furminger.mycomponents.TitleLabel;
+
+public class MatchThree extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	
+	private ScorePanel scorePanel = new ScorePanel(0, Color.GREEN);
+	private BallPanel ballPanel = new BallPanel(this);
+
+	public static void main(String[] args) {
+		try {
+			String className = UIManager.getCrossPlatformLookAndFeelClassName();
+			UIManager.setLookAndFeel(className);
+		}
+		catch (Exception e) {}
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				new MatchThree();
+			}
+		});
+		
+	}
+	
+	public MatchThree() {
+		initGUI();
+		setTitle("MatchThree");
+		setResizable(false);
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	private void initGUI() {
+		TitleLabel titleLabel = new TitleLabel("Match Three");
+		add(titleLabel, BorderLayout.PAGE_START);
+		
+		// main panel
+		JPanel mainPanel = new JPanel();
+		mainPanel.setBackground(Color.GREEN);
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+		add(mainPanel, BorderLayout.CENTER);
+		
+		// score panel
+		mainPanel.add(scorePanel);
+		
+		// ball panel
+		mainPanel.add(ballPanel);
+		
+		// button panel
+		
+	}
+
+}
