765d444f6fd0ac0a45b7790cdc917a8e15404f45
Jonathan Furminger <58958324+jonathanfurminger@users.noreply.github.com>
2022-02-14 14:17:17 +0000

    Match Three 3-60

--- MatchThree/src/jonathan/furminger/matchthree/BallPanel.java
+++ MatchThree/src/jonathan/furminger/matchthree/BallPanel.java
@@ -56,6 +56,9 @@ public class BallPanel extends JPanel {
 		for(int row = 0; row < ROWS; row++) {
 			for(int col = 0; col < COLS; col++) {
 				cells[row][col] = new Cell();
+				while(twoMatchAbove(row, col) || twoMatchToLeft(row, col)) {
+					cells[row][col] = new Cell();
+				}
 			}
 		}
 		repaint();
@@ -120,12 +123,46 @@ public class BallPanel extends JPanel {
 	}
 	
 	private void swap(int row1, int col1, int row2, int col2) {
-		Cell temp = new Cell();
-		temp.copy(cells[row1][col1]);
-		cells[row1][col1].copy(cells[row2][col2]);
-		cells[row2][col2].copy(temp);
-		repaint();
-		
+		new Thread (new Runnable() {
+			public void run() {
+				boolean validSwap =false;
+				
+				Cell temp = new Cell();
+				temp.copy(cells[row1][col1]);
+				cells[row1][col1].copy(cells[row2][col2]);
+				cells[row2][col2].copy(temp);
+				
+				int points = 0;
+				boolean repeat = true;
+				while(repeat) {
+					points = 0;
+					for(int row = 0; row < ROWS; row++) {
+						int newPoints = markChainsAndGetPointsInRow(row);
+						points += newPoints;
+					}
+					
+					for(int col = 0; col < COLS; col++) {
+						int newPoints = markChainsAndGetPointsInCol(col);
+						points += newPoints;
+					}
+					repaint();
+					if(points > 0) {
+						validSwap = true;
+						game.addToScore(points);
+						removeMarkedChains();
+					}
+					else {
+						repeat = false;
+					}
+				}
+				
+				// if not a valid swap, put swapped balls back
+				if(!validSwap) {
+					cells[row2][col2].copy(cells[row1][col1]);
+					cells[row1][col1].copy(temp);
+				}
+			}
+		}).start();
 	}
 	
 	private void clicked(int x, int y) {
@@ -165,4 +202,187 @@ public class BallPanel extends JPanel {
 		}
 	}
 
+	private boolean twoMatchAbove(int row, int col) {
+		boolean match = false;
+		if(row > 1 && row < ROWS && col >= 0 && col < COLS) {
+			int color1 = cells[row][col].getColor();
+			int color2 = cells[row-1][col].getColor();
+			int color3 = cells[row-2][col].getColor();
+			if(color1 == color2 && color1 == color3) {
+				match = true;
+			}
+		}
+		return match;
+	}
+	
+	private boolean twoMatchToLeft(int row, int col) {
+		boolean match = false;
+		if(row >= 0 && row < ROWS && col > 1 && col < COLS) {
+			int color1 = cells[row][col].getColor();
+			int color2 = cells[row][col-1].getColor();
+			int color3 = cells[row][col-2].getColor();
+			if(color1 == color2 && color1 == color3) {
+				match = true;
+			}
+		}
+		return match;
+	}
+	
+	private int markChainsAndGetPointsInRow(int row) {
+		int points = 0;
+		
+		// get the color of the first ball
+		int color = cells[row][0].getColor();
+		int count = 1;
+		
+		// loop through each column of the row
+		for(int col = 1; col < COLS; col++) {
+			// count the balls in each color
+			int nextColor = cells[row][col].getColor();
+			if(nextColor == color) {
+				count++;
+			}
+			else {
+				color = nextColor;
+				points += calculatePoints(count);
+				count = 1;
+			}
+			
+			//mark chains of three or more. When count
+			// reaches 3, also mark previous two.
+			if(count == 3) {
+				cells[row][col].setInChain(true);
+				cells[row][col-1].setInChain(true);
+				cells[row][col-2].setInChain(true);;
+			}
+			else if(count > 3) {
+				cells[row][col].setInChain(true);
+			}
+			
+		}
+		points += calculatePoints(count);
+		return points;
+	}
+	
+
+	private int markChainsAndGetPointsInCol(int col) {
+		int points = 0;
+		
+		// get the color of the first ball
+		int color = cells[0][col].getColor();
+		int count = 1;
+		
+		// loop through each row of the column
+		for(int row = 1; row < ROWS; row++) {
+			// count the balls in each color
+			int nextColor = cells[row][col].getColor();
+			if(nextColor == color) {
+				count++;
+			}
+			else {
+				color = nextColor;
+				points += calculatePoints(count);
+				count = 1;
+			}
+			
+			//mark chains of three or more. When count
+			// reaches 3, also mark previous two.
+			if(count == 3) {
+				cells[row][col].setInChain(true);
+				cells[row-1][col].setInChain(true);
+				cells[row-2][col].setInChain(true);;
+			}
+			else if(count > 3) {
+				cells[row][col].setInChain(true);
+			}
+			
+		}
+		points += calculatePoints(count);
+		return points;
+	}
+	
+	private int calculatePoints(int count) {
+		int points = 0;
+		if(count == 3) {
+			points = 10;
+		}
+		else if(count == 4) {
+			points = 15;
+		}
+		else if(count >= 5) {
+			points = 20;
+		}
+		return points;
+	}
+	
+	private void removeMarkedChains() {
+		// pause before clearing the marked chains
+		pause(1000);
+		
+		// remove marked chains
+		for(int row = 0; row < ROWS; row++) {
+			for(int col = 0; col < COLS; col++) {
+				if(cells[row][col].isInChain() ) {
+					cells[row][col].setEmpty();
+				}
+			}
+		}
+		repaint();
+		pause(500);
+		
+		// loop through all rows and columns,
+		// starting with the last row
+		for(int row = ROWS - 1; row >= 0; row--) {
+			boolean foundEmptyInCol = false;
+			for(int col = 0; col < COLS; col++) {
+				
+				// if a cell is empty, copy the first non-empty
+				// cell above it
+				if(cells[row][col].isEmpty()) {
+					foundEmptyInCol = true;
+					boolean foundBall = false;
+					for(int r = row - 1; r >= 0 && !foundBall; r--) {
+						if(!cells[r][col].isEmpty()) {
+							cells[row][col].copy(cells[r][col]);
+							cells[r][col].setEmpty();
+							foundBall = true;
+						}
+					}
+					// if no call was found above it,
+					// fill the cell with a new ball
+					if(!foundBall) {
+						cells[row][col] = new Cell();
+					}
+				}
+
+			}
+			
+			// repaint and pause after each row
+			// so the user can see one row drop
+			// at a time
+			repaint();
+			if(foundEmptyInCol) {
+				pause(500);
+			}
+		}
+	}
+	
+	private void pause(int milliseconds) {
+		try {
+			Thread.sleep(milliseconds);
+		}
+		catch (InterruptedException e) {}
+	}
+	
+	private boolean inChain(int row, int col) {
+		boolean inChain = twoMatchAbove(row, col)
+				|| twoMatchAbove(row + 1, col)
+				|| twoMatchAbove(row + 2, col)
+				|| twoMatchToLeft(row, col)
+				|| twoMatchToLeft(row, col + 1)
+				|| twoMatchToLeft(row, col + 2);
+		
+		return inChain;
+	}
+	
 }
