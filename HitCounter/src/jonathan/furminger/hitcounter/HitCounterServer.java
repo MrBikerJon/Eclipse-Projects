4c8f62ea36352dfbd24a47a5350274ea3735be84
Jonathan Furminger <58958324+jonathanfurminger@users.noreply.github.com>
2022-02-19 20:45:50 +0000

    Started Hit Counter 1 - 5 Deleted Blitz

---
+++ HitCounter/src/jonathan/furminger/hitcounter/HitCounterServer.java
@@ -0,0 +1,38 @@
+package jonathan.furminger.hitcounter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class HitCounterServer {
+
+	private static final int PORT_NUMBER = 63457;
+	
+	private ServerSocket serverSocket;
+	private Socket socket;
+	private int count = 0;
+	
+	public static void main(String[] args) {
+
+		new HitCounterServer();
+		
+	}
+	
+	public HitCounterServer() {
+		System.out.println("The server is running");
+		try {
+			serverSocket = new ServerSocket(PORT_NUMBER);
+			socket = serverSocket.accept();
+			System.out.println("Starting new connection.");
+			PrintWriter toClient = new PrintWriter(socket.getOutputStream(), true);
+			count++;
+			toClient.println("" + count);
+		}
+		catch(IOException e) {
+			System.out.println("An exception was caught when trying to listen on port " + PORT_NUMBER + " or when listening for a connection.");
+		}
+		
+	}
+
+}
