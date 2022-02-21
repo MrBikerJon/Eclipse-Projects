fc56f3de3efec8a4e5afa84d3a2a257164560689
Jonathan Furminger <58958324+jonathanfurminger@users.noreply.github.com>
2022-02-21 17:07:27 +0000

    chat 2-36

--- Chat/src/jonathan/furminger/chat/Chat.java
+++ Chat/src/jonathan/furminger/chat/Chat.java
@@ -1,14 +1,25 @@
 package jonathan.furminger.chat;
 
 import java.awt.BorderLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ConnectException;
 import java.net.Socket;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
@@ -25,6 +36,8 @@ public class Chat extends JFrame implements Runnable {
 	private Socket socket;
 	private BufferedReader in;
 	private PrintWriter out;
+	private JTextArea chatArea = new JTextArea(20, 20);
+	private JTextArea inputArea = new JTextArea(3, 20);
 
 	public static void main(String[] args) {
 		try {
@@ -56,6 +69,47 @@ public class Chat extends JFrame implements Runnable {
 	private void initGUI() {
 		TitleLabel titleLabel = new TitleLabel("Chat");
 		add(titleLabel, BorderLayout.PAGE_START);
+		
+		// listeners
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				close();
+			}
+		});
+		
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+		add(mainPanel);
+		
+		// chat area
+		chatArea.setEditable(false);
+		chatArea.setLineWrap(true);
+		chatArea.setWrapStyleWord(true);
+		Insets marginInsets = new Insets(3, 3, 3, 3);
+		chatArea.setMargin(marginInsets);
+		JScrollPane chatScrollPane = new JScrollPane(chatArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		mainPanel.add(chatScrollPane);
+		
+		// input area
+		JLabel messageLabel = new JLabel("Type your message here");
+		mainPanel.add(messageLabel);
+		inputArea.setLineWrap(true);
+		inputArea.setWrapStyleWord(true);
+		inputArea.setMargin(marginInsets);
+		JScrollPane inputScrollPane = new JScrollPane(inputArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		mainPanel.add(inputScrollPane);
+		
+		// button panel
+		JPanel buttonPanel = new JPanel();
+		add(buttonPanel, BorderLayout.PAGE_END);
+		JButton sendButton = new JButton("Send");
+		
+		sendButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				send();
+			}
+		});
+		buttonPanel.add(sendButton);
 	}
 	
 	public void run() {
@@ -84,6 +138,14 @@ public class Chat extends JFrame implements Runnable {
 		}
 		catch (Exception e) {}
 		System.exit(0);
+	}
+	
+	private void send() {
+		String message = inputArea.getText().trim();
+		if(message.length() > 0) {
+			System.out.println(message);
+			inputArea.setText("");
+		}
 	}
 
 }
