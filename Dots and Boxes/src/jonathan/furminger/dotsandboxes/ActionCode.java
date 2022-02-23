05f4c33256d89bc8be9ed61b2c7f0807991a22c9
Jonathan Furminger <58958324+jonathanfurminger@users.noreply.github.com>
2022-02-23 20:00:08 +0000

    Dots and Boxes 3-21

---
+++ Dots and Boxes/src/jonathan/furminger/dotsandboxes/ActionCode.java
@@ -0,0 +1,21 @@
+package jonathan.furminger.dotsandboxes;
+
+public class ActionCode {
+
+	// server to client
+	public static final String SUBMIT = "SU";
+	public static final String ACCEPTED = "AC";
+	public static final String REJECTED = "RE";
+	public static final String OPPONENT = "OP";
+	
+	// client to server
+	public static final String NAME = "NA";
+
+	// client to client
+	public static final String MESSAGE = "ME";
+	public static final String MY_TURN = "MT";
+	public static final String LINE = "LI";
+	public static final String QUIT = "QU";
+
+
+}
---
+++ Dots and Boxes/src/jonathan/furminger/dotsandboxes/DotsAndBoxes.java
@@ -0,0 +1,92 @@
+package jonathan.furminger.dotsandboxes;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ConnectException;
+import java.net.Socket;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+import jonathan.furminger.networking.LogInDialog;
+
+public class DotsAndBoxes extends JFrame implements Runnable {
+
+	private static final long serialVersionUID = 1L;
+	private static final int PORT_NUMBER = 49632;
+	
+	private LogInDialog logInDialog = new LogInDialog("Dots and Boxes");
+	private String host = "";
+	private String name = "";
+	private Socket socket;
+	private BufferedReader in;
+	private PrintWriter out;
+	private boolean keepRunning = true;
+			
+	public DotsAndBoxes() {
+		logIn();
+		new Thread(this);
+	}
+	
+	public static void main(String[] args) {
+		try {
+			String className = UIManager.getCrossPlatformLookAndFeelClassName();
+			UIManager.setLookAndFeel(className);
+		}
+		catch (Exception e) {}
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new DotsAndBoxes();
+			}
+		});
+
+	}
+
+	private void logIn() {
+		logInDialog.setVisible(true);
+		if(!logInDialog.isCanceled()) {
+			host = logInDialog.getIpAddress();
+			name = logInDialog.getName();
+		}
+		else {
+			close();
+		}
+	}
+	
+	public void run() {
+		try {
+			socket = new Socket(host, PORT_NUMBER);
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out = new PrintWriter(socket.getOutputStream(), true);
+			
+			keepRunning = true;
+			while(keepRunning) {
+				String input = in.readLine();
+				if(input == null) {
+					keepRunning = false;
+				}
+			}
+		}
+		catch (ConnectException e) {
+			JOptionPane.showMessageDialog(this, "The server is not running");
+		}
+		catch (IOException e) {
+			JOptionPane.showMessageDialog(this, "The connection to the server was lost");
+		}
+		finally {
+			close();
+		}
+	}
+	
+	public void close() {
+		System.exit(0);
+	}
+	
+
+}
